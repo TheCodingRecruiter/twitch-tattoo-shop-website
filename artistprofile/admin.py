e2a94c2f5be3c4862904f35e57a54c5c9e655226
Richard Eby <richard.eby@hotmail.com>
2020-01-08 00:22:02 -0500

    Fucked up my whole project tonight, but started over and got one freaking thing done after three hours of work

--- artistprofile/admin.py
+++ artistprofile/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 from .models import ArtistProfile
 # Register your models here.
 
-admin.site.register(ArtistProfile)
+class artistAdmin(admin.ModelAdmin):
+    list_display = ['artist', 'email', 'phone', 'tattoo_artist', 'piercing_artist']
+
+admin.site.register(ArtistProfile, artistAdmin)
--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-admin.site.register(Post)
+class postAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+
+admin.site.register(Post, postAdmin)
